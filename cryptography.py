921a93f5f279738f7ab4129bf5de7d59ab54556b
sarahdunbar <sarahdunbar@hanovernorwichschools.org>
2015-10-02 14:56:17 -0400

    Updated from Brython Server: 10/2/2015 2:56:17 PM

--- cryptography.py
+++ cryptography.py
@@ -10,16 +10,46 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 
+def cryptolist(letter2, key2):
+    r = len(letter2)
+    k = len(key2)
+    if r > k:
+        range1 = r
+        listreal = letter2[:]
+    if r < k:
+        range1 = k
+        listreal = key2[:]
+    if r = k:
+        range1 = k
+        listreal = key2[:]
+    for x in range(0, range1):
+        if x > r:
+            x = x - r - 1
+        if x > k:
+            x = x - k - 1
+        valuem = letter2[x]
+        valuen = key2[x]
+        valueg = valuem + valuen
+        listreal[x] = valueg
+    
+
 def efunction (i, m, k):
     letters = list(m)
+    letterkey = list(k)
     r = len(letters)
+    k = len(letterkey)
     letter2 = letters[:]
-    print(letters)
+    key2 = letterkey[:]
     for x in range (0, r):
         p = letters[x]
         l = associations.find(p)
         letter2[x] = l
-    print(letter2)
+    for x in range (0, k):
+        p = letterkey[x]
+        l = associations.find(p)
+        key2[x] = l
+    cryptolist(letter2, key2)
+
        
        
        
